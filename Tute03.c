583dd68fa12252cd69e712a40429f8f1f5ce95f5
Rishitha055 <127425850+Rishitha055@users.noreply.github.com>
2023-03-16 00:56:30 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -1,19 +1,19 @@
 /*Exercise 3 - Repetition
 
 Write a C program to calculate the sum of the numbers from 1 to n.
-Where n is a keyboard input.
+Where n is a keyboard input.*/
 
-e.g.
-
-n -> 100
-sum = 1+2+3+....+ 99+100 = 5050
-
-n -> 1-
-sum = 1+2+3+...+10 = 55 */
-
-#include <stdio.h>
-int main() {
-  
-  return 0;
+#include<stdio.h>
+int main(void)
+{
+	int num,sum=0,i;
+	printf("Enter the value: ");
+	scanf_s("%d", &num);
+	for (i = 1;i <= num;i++)
+	{
+		sum = sum + i;
+	}
+	printf("Sum is: %d", sum);
+	return 0;
 }
 
